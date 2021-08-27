bae6fef9693d9f5d7b4865f8888f646254445821
dagmawiG8 <dagiwold@gmail.com>
2021-08-27 22:25:32 +0300

    using stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",50,1);
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",50);
 
 	return (1);
 }

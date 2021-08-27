eeea6f050a1dc1b6bb04e2f40da0a076fa50617f
dagmawiG8 <dagiwold@gmail.com>
2021-08-27 20:24:33 +0300

    the c programme using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Description: prints text using printf
+ * Return: returns 0 for successful execution
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+}

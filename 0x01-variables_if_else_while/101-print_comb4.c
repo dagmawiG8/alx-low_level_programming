27b0e4fff62f22d99fa70a17dce6fbd3313848d3
dagmawiG8 <dagiwold@gmail.com>
2021-08-29 00:58:25 +0300

    triplet combination of the single digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry
+ * Description: all available paired combination of digits
+ * Return: returns zero after printing all
+ */
+
+int main(void)
+{
+	int digit1;
+	int digit2;
+	int digit3;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+			}
+
+			if ((digit1 != 7) || (digit2 != 8) || (digit3 != 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

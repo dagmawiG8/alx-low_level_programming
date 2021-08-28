1fff3e039f20692ccff99810a6463338fc5351a7
dagmawiG8 <dagiwold@gmail.com>
2021-08-28 11:27:31 +0300

    positivity

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if (n>0)
+	{
 		printf("%d is negative", n);
-	else if (n<0)
+	}else if (n<0)
+	{
 		printf("%d is positive", n);
-	else 
+	}else
+	{ 
 		printf("%d is zero", n);
-
+	}
 	return (0);
 }

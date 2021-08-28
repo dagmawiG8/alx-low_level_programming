16e3e7a730d0f17aa7bf2a460df252b4ded840ff
dagmawiG8 <dagiwold@gmail.com>
2021-08-29 01:19:42 +0300

    pair-pair combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,22 +12,6 @@ int main(void)
 	int i;
 	int j;
 
-/**     for (digit1 = 0; digit1 < 10; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar(digit1 + '0');
-			putchar(digit2 + '0');
-
-			if ((digit1 != 7) || (digit2 != 8) || (digit3 != 9))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-*/
-
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
@@ -39,7 +23,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-		
+
 				if (i != 98 || j != 99)
 				{
 					putchar(',');

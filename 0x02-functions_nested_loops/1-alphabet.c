4632962477db0812f360899de5065dcf3c1f2306
dagmawiG8 <dagiwold@gmail.com>
2021-08-31 16:54:36 +0300

    re-printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - entry
+ * Desciption: printing alphabets.
+ * Return: On success 0.
  */
 
 int main (void)

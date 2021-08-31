004439caceefcc77dabbf26655bc008237a65959
dagmawiG8 <dagiwold@gmail.com>
2021-08-31 17:06:44 +0300

    re-printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,15 @@
 
 int main (void)
 {
-	print_alphabet();
+	void print_alphabet(void)
+	{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);	
+		}
+	}
 
 	return (0);
 }

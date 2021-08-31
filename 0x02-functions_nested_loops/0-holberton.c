984b45ee78ed02ea00ea520fb53d2fee0bd144c5
dagmawiG8 <dagiwold@gmail.com>
2021-08-31 15:49:45 +0300

    prototype of putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry
+ * Description: Prints Holberton using putchar's prototype.
+ * Return: returns-0
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+
+	return (0);
+}

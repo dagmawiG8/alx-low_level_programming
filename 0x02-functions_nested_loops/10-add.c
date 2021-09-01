49713325efa5eae27e10a79e8db1af68dd4373b9
dagmawiG8 <dagiwold@gmail.com>
2021-09-01 15:37:00 +0300

    9-time_table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - adds two integers
+ * @x: The character to add on
+ * @y: The charater to be added
+ * Description: displaying sum of two numbers
+ * Return: returns an int value
+ */
+
+int add(int x, int y)
+{
+	return (x + b);
+}

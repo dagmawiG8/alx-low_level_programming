a707318ab6590d616237344c63e525c14f50c99c
dagmawiG8 <dagiwold@gmail.com>
2021-09-01 11:31:27 +0300

    test sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,15 +6,24 @@
  * print_sign - test for character or not
  * @n: the character to be tested
  * Desciption: test for alphanumeric
- * Return: On success 1.
+ * Return: On success 1 , 0 and -1.
  */
 
 int print_sign(int n)
 {
-        if (n > 0)
-                return (1);
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
         else if (n < 0)
-                return (-1);
-	else 
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf("0");
 		return (0);
+	}
 }

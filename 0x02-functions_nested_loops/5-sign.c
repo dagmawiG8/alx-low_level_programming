028fb7f2b072e88c8676f6b00a588fecab055483
dagmawiG8 <dagiwold@gmail.com>
2021-09-01 11:26:46 +0300

    test sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_sign - test for character or not
+ * @n: the character to be tested
+ * Desciption: test for alphanumeric
+ * Return: On success 1.
+ */
+
+int print_sign(int n)
+{
+        if (n > 0)
+                return (1);
+        else if (n < 0)
+                return (-1);
+	else 
+		return (0);
+}

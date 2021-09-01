05273195fdb18341978f4aab95e17c0468d1cf80
dagmawiG8 <dagiwold@gmail.com>
2021-09-01 12:06:10 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_last_digit - test for character or not
+ * @n: the character to be tested
+ * Desciption: test for alphanumeric
+ * Return: On success 1 , 0 and -1.
+ */
+
+int print_last_digit(int n)
+{
+	return (n%10);
+}

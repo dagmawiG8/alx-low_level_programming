3868d89517955570535cbb87c8409f9b4e48f8d4
dagmawiG8 <dagiwold@gmail.com>
2021-09-01 15:28:26 +0300

    9-time_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
- * times_table -
+ * times_table - prints factors of 9
  * @c: The character to print
- * Description:
- * Return: 
+ * Description: displaying table of 9s table
+ * Return: no return value
  */
 
-void times_table()
+void times_table(void)
 {
 	int x, y;
 
 	for (x = 0; x < 10; x++)
 	{
-		for(y = 0; y < 10; y++)
+		for (y = 0; y < 10; y++)
 		{
 			_putchar((x * y) + '0');
 			_putchar(',');

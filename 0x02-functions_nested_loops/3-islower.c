d9225480db36ea419b7b9e8b1569b1b3cfb92db6
dagmawiG8 <dagiwold@gmail.com>
2021-09-01 10:17:34 +0300

    testing for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - test for lowercase
+ * Desciption: printing alphabets 10 times
+ * Return: On success 1.
+ */
+
+int _islower(int c)
+{
+	int tester;
+
+	tester = islower(c);
+
+	return tester;
+}

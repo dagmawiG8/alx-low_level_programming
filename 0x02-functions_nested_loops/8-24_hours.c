6ec4b4f147ba7171c850a700770c5ffd53e9f1d9
dagmawiG8 <dagiwold@gmail.com>
2021-09-01 14:43:58 +0300

    hour:minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,7 @@
 
 /**
  * jack_bauer - writes the character c to stdout
- * 
- * Description: 
+ * Description:
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -13,11 +12,11 @@ void jack_bauer(void)
 {
 	int w, x, y, z;
 
-	for (w = 0; w < 10; w++)
+	for (w = 0; w < 3; w++)
 	{
-		for (x = 0; x < 10; x++)
+		for (x = 0; x < 4; x++)
 		{
-			for (y = 0; y < 10; y++)
+			for (y = 0; y < 6; y++)
 			{
 				for (z = 0; z < 10; z++)
 				{

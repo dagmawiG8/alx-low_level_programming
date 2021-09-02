e515e4cadda43044fd54ec83a4f319f5d3b7aa3e
dagmawiG8 <dagiwold@gmail.com>
2021-09-02 23:51:50 +0300

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int intermediate;
+
+	if (n < 0)
+	{
+		intermediate = -n;
+		_putchar('-');
+	} else
+	{
+		intermediate = n;
+	}
+
+	print_number(n1 / 10);
+}

2f85112da57ac1e88442e3918efffe307b54ebc9
dagmawiG8 <dagiwold@gmail.com>
2021-09-02 12:14:39 +0300

    sum of even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int a = 1;
+	long int b = 2;
+	long int sum = 2; /* The first even */
+	long int next;
+
+        while (next < 4000)
+	{
+		next = a + b;
+
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+		
+		a = b;
+		b = next;
+	}
+	
+	pritnf("%ld", sum)
+	printf("\n");
+	return (0);
+}

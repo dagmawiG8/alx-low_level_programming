93b513991c575d5d98d302a6b8deb6d8d9bc8ade
dagmawiG8 <dagiwold@gmail.com>
2021-09-02 11:35:29 +0300

    sum of the multiples

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int counter = 3
+
+	printf("%d, ", a);
+        printf("%d, ", b);
+
+	while (counter <= 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+
+		printf("%d, ", next);
+	}
+	
+	printf("\n");
+	return (0);
+}

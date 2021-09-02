84f5d804da6dffddcb9189d18b3d89500c080779
dagmawiG8 <dagiwold@gmail.com>
2021-09-02 10:54:42 +0300

    table of n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,24 +5,22 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
-
-
 void print_times_table(int n)
 {
 	int x, y;
 
-	for (x = 0; x <= n; x++)
-        {
-		for (y = 0; y <= n; y++)
+	if (n <= 15 && n > 0)
+	{
+		for (x = 0; x <= n; x++)
 		{
-			if (n <= 15 && n > 0)
+			for (y = 0; y <= n; y++)
 			{
 				if (y == 0)
 				{
 					printf("%d", (x * y));
 				}
 
-				else if (y != 0)
+				else
 				{
 					if ((x * y) < 10)
 					{
@@ -51,4 +49,3 @@ void print_times_table(int n)
 		}
 	}
 }
-

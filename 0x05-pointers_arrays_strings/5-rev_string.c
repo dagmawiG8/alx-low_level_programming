a93081ab6a2ac1609f4d0a7ac0427cd91a13559d
dagmawiG8 <dagiwold@gmail.com>
2021-09-07 01:11:19 +0300

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,19 +11,22 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len, Ilen;
+	int i, len, len1;
 
-	len = 0, Ilen = len - 1;
+	len = 0;
+	len1 = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
+	len1 = len - 1;
+
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[Ilen];
-		s[Ilen--] = tmp;
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }

eb7f7927998588676529ab1c33c70a606cf74c69
dagmawiG8 <dagiwold@gmail.com>
2021-09-10 21:32:23 +0300

    print buffer chars

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * print_line - prints a s bytes of a buffer
  * @c: buffer to print
  * @s: bytes of buffer to print
  * @l: line of buffer to print
- *
  * Return: void
  */
 

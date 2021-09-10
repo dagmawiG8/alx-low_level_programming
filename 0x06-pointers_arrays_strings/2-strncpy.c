5b1fa369574631033489bce706074eaaf2c8a59e
dagmawiG8 <dagiwold@gmail.com>
2021-09-10 15:48:59 +0300

    string cpy with n bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,16 +4,28 @@
 #include <string.h>
 
 /**
- * _strncpy -
- * @dest, @src: an argument character
- * Return: char pointer
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ * Return: pointer to the resulting string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-        char *result;
-        *result = strncpy(*dest, *src, n);
+	int i = 0;
 
-        return (result);
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
-

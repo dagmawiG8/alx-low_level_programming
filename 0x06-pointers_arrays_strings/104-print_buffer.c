bf55b7b0ae9d85972ad850e04bddb2452c21e23d
dagmawiG8 <dagiwold@gmail.com>
2021-09-10 21:22:30 +0300

    print buffer chars

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,12 +3,10 @@
 
 /**
  * print_line - prints a s bytes of a buffer
- * print_buffer - prints a buffer
- * @b: buffer to print
  * @c: buffer to print
  * @s: bytes of buffer to print
  * @l: line of buffer to print
- * @size: size of buffer
+ *
  * Return: void
  */
 
@@ -34,7 +32,13 @@ void print_line(char *c, int s, int l)
 	}
 }
 
-
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
 	int i;
@@ -45,7 +49,7 @@ void print_buffer(char *b, int size)
 		if (i < size / 10)
 		{
 			print_line(b, 9, i);
-			}
+		}
 		else
 		{
 			print_line(b, size % 10 - 1, i);

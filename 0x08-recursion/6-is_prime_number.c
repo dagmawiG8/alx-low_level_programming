d4c04f7823adc120a64f074d4db49adfd41c2103
dagmawiG8 <dagiwold@gmail.com>
2021-09-19 10:08:20 +0300

    tests for prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-int fakefake(int prime, int primediv);
+
+/**
+ * function Prototype
+ */
+
+int is_prime_func(int prime, int primediv);
+
 /**
  * is_prime_number - get prime number of given number
  * @n: is a natural number
@@ -9,26 +15,24 @@ int fakefake(int prime, int primediv);
 
 int is_prime_number(int n)
 {
-return (fakefake(n, 3));
+	return (is_prime_func(n, 3));
 }
 
 /**
- * fakefake - get the prime number
- * @prime: is a natural number
- * @primediv: is a natural number
+ * is_prime_func - get the prime number
+ * @num: is a natural number
+ * @test: is a natural number
  * Return: 1 if n is a prime number and 0 if not.
  */
 
-int fakefake(int prime, int primediv)
+int is_prime_func(int num, int test)
 {
-	if (n <= 2)
-        return (n == 2) ? true : false;
-    if (n % i == 0)
-        return false;
-    if (i * i > n)
-        return true;
- 
-    // Check for next divisor
-    return isPrime(n, i + 1);
+	if (num <= 1 || (num % test == 0 && test <= num / 2))
+		return (0);
 
+	else if (num % test != 0 && test > num / 2)
+		return (1);
+
+	else
+		return (is_prime_func(num, test + 1));
 }

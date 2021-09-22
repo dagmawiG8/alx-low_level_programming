e9ab3896c210929245035e5124f5ae4011764083
dagmawiG8 <dagiwold@gmail.com>
2021-09-22 21:43:43 +0300

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * main - print the number of argument
@@ -7,10 +8,10 @@
   * Return: 0 to indicate a good working of the program
   */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if(*argv != '\0')
-		printf("%d\n", argc - 1);
+	if (**argv != '\0')
+	printf("%d\n", argc - 1);
 
 	return (0);
 }

026bab76d6ed4146b575243637ee3d8ea123818e
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-09-26 17:46:16 +0300

    Update 2-str_concat.c
    updating q2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,17 +12,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
+
 	unsigned int long i = 0;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	else if (s2 == NULL)
+
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-	else
+
+	if (s1 != NULL || s2 != NULL)
 	{
 		str = malloc((sizeof(s1) + sizeof(s2)));
 		if (str == NULL)

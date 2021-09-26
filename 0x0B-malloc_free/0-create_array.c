d28fe5db2eb7a40a2f3f818d601811ef9f4d31b8
dagmawiG8 <dagiwold@gmail.com>
2021-09-26 14:36:55 +0300

    creating array and initializing it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * create_array - fills memory with a constant byte
+  * @size: is the size of the pointer
+  * @c: is a letter
+  * Return - NULL or a pointer
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i = 0;
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (i < size)
+		{
+			str[i] = c;
+			i++;
+		}
+	}
+	return (s);
+}

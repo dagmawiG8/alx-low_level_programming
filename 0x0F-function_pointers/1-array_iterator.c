4afe24a89873354fd47984d14b6815e808b0391d
dagmawiG8 <dagiwold@gmail.com>
2021-10-01 19:53:05 +0300

    function pointer revealed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,16 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (array = NULL || action = NULL || size = 0)
-	{
 		return;
-	}
 
-	while (i < size)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
-		i++;
 	}
 }

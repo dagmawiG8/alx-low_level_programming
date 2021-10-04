f4c2bcfd4edcc5a146976177eefd2790ecb2a0a1
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-04 10:52:49 +0300

    updating 1-print numbers with separators

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,33 +7,18 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	char *str;
-
 	va_list li;
+	unsigned int i;
 	va_start(li, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(li, char *);
-		if (str == NULL)
-		{
-			return;
-		}
+		printf("%d", va_arg(gett, int));
 
-		if (separator == NULL)
-		{
-			printf("%s", str);
-		}
-		else if (separtor != NULL && i = 0)
-		{
-			printf("%s", str);
-		}
-		else
-		{
-			printf("%s%s", separator, str);
-		}
+		if (separator != NULL && i != 0)
+			printf("%s", separator);
 	}
+
 	printf("\n");
 	var_end(li);
 }

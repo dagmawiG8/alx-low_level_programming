521a31be694f46ef90244590cb08b265fa235755
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-04 10:57:24 +0300

    updating print numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,11 +9,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list li;
 	unsigned int i;
+
 	va_start(li, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(gett, int));
+		printf("%d", va_arg(li, int));
 
 		if (separator != NULL && i != 0)
 			printf("%s", separator);

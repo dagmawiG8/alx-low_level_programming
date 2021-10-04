b760d4834533286c6a209bf7a2769eda9488a599
dagmawiG8 <dagiwold@gmail.com>
2021-10-04 10:45:48 +0300

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,9 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
+#ifndef VARIADIC_F
+#define VARIADIC_F
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdring.h>
 #include <stdlib.h>
-
-/**
-  * struct op - Struct op
-  *
-  * @op: The operator
-  * @f: The function associated
-  */
-
-typedef struct op
-{
-	char *op;
-	void (*f)(va_list);
-} op_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);

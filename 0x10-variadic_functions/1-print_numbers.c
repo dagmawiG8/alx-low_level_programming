45bf3d6ba2e89144b08341b315c139b5d68d7b72
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-04 11:12:44 +0300

    areee

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,12 +14,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(list, int));
-
-		if (separator != NULL && i != n - 1)
-		{
-			printf("%s", separator);
-		}
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
 
 	var_end(list);

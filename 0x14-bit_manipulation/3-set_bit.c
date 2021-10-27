4aff9601c232c17e4211b7b644e8a41af7c04a66
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-27 15:53:32 +0300

    updating set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,8 +10,8 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > (8 * sizeof(*n))
-		return (-1);
+	    return (-1);
 
 	*n |= (1 << index);
-	return (1);
+	    return (1);
 }

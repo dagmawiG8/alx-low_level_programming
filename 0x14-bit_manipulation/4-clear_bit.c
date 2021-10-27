26e0edd4fb2a6e6b3fb9b8e5628bca8e1493cc7e
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-27 14:45:06 +0300

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * clear_bit - sets a bit at index to zero
+  * @n: pointer to number
+  * @index: index to change
+  * Return: 1 on success, -1 on failure
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+  if (index > (8 * sizeof(*n)))
+    return (-1);
+  
+  *n &= (1 << index);
+    return (1);

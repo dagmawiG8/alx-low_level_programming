e4d781760fa83d0d86a8a26b9d91947d58bc2237
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-27 15:26:55 +0300

    updating flip_bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,12 +10,12 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-  int count = 0;
+  unsigned int count = 0;
 
   while (n > 0 || m > 0)
   {
-    int t1 = (n&1);
-    int t2 = (m&1);
+    unsigned int t1 = (n&1);
+    unsigned int t2 = (m&1);
  
     if (t1 != t2){
       count++;

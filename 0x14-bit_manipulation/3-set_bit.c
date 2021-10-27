834af40007e8bbe6722a2004f1050a2a0c72fa8b
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-27 14:32:27 +0300

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_bit - sets bit at index to 1
+  * @n: pointer to number
+  * @index: how many places to shift
+  * Return: 1 on success, -1 on failure
+  */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 11)
+		return (-1);
+
+	*n |= (1 << index);
+	return (1);
+}

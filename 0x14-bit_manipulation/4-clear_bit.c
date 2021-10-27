957e6f83f353157b2246555cdf1f5992cec1eb6d
Dagmawi Gebrewold <81195616+dagmawiG8@users.noreply.github.com>
2021-10-27 15:54:34 +0300

    updating clear bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
   if (index > (8 * sizeof(*n)) - 1)
     return (-1);
   
-  *n &= (1 << index);
+  *n &= ~(1 << index);
     return (1);
 }
